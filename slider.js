26461a486315c9683de4b92ebc29f45a29d3818e
JimmyCasino <muratdemirr.355@gmail.com>
2025-01-21 04:42:53 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -3,20 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = [
         { src: "img/banner/banner1.png", link: "https://betebak.ws/OrTCy" },
         { src: "img/banner/banner2.png", link: "https://betebak.ws/OrTCy" },
-        { src: "img/banner/banner3.png", link: "https://betebak.ws/OrTCy" },
-        { src: "img/banner/banner4.png", link: "https://betebak.ws/OrTCy" },
-        { src: "img/banner/banner5.png", link: "https://betebak.ws/OrTCy" },
-        { src: "img/banner/banner6.png", link: "https://betebak.ws/OrTCy" },
-        { src: "img/banner/banner7.png", link: "https://betebak.ws/OrTCy" },
-        { src: "img/banner/banner8.png", link: "https://betebak.ws/OrTCy" },
-        { src: "img/banner/banner9.png", link: "https://betebak.ws/OrTCy" },
-        { src: "img/banner/banner10.png", link: "https://betebak.ws/OrTCy" }
+        { src: "img/banner/banner3.png", link: "https://betebak.ws/OrTCy" }
     ];
     let currentIndex = 0;
     
     function setInitialBannerBackground() {
         bannerSection.style.backgroundImage = `url('${images[0].src}')`;
-        // İlk resim için tıklama işlevini ayarla
         bannerSection.onclick = () => {
             window.open(images[0].link, '_blank');
         };
